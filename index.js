5c7368e4a21686fab0902dfca5f78ae717ffbfe8
markiplier <milkybuiscet@gmail.com>
2025-01-20 09:20:54 +0300

    Updated portfolio

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX / window.innerWidth;
+    const y = event.clientY / window.innerHeight;
 
+    document.body.style.background = `radial-gradient(circle at ${x * 100}% ${y * 100}%, #f3f4f6, #e8eaed)`;
+  });
   
--- project3.js
+++
@@ -1,8 +0,0 @@
-const background = document.querySelector('.background');
-
-document.addEventListener('mousemove', (e) => {
-  const x = e.clientX / window.innerWidth;
-  const y = e.clientY / window.innerHeight;
-
-  background.style.backgroundPosition = `${x * 100}% ${y * 100}%`;
-});
