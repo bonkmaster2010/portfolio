5393796d0a008d3c523e94c1db500607008d7dfb
bonkmaster2010 <milkybuiscet@gmail.com>
2025-06-12 06:02:08 +0300

    made it easier to close the other projects modal and added a back btn (btn is short for button btw)

--- portfolio/src/components/OtherProjects.tsx
+++ portfolio/src/components/OtherProjects.tsx
@@ -1,12 +1,14 @@
 import Card from './Card';
 import ImgPreview from './imagesPreview';
 import Footer from './Footer';
+import { useNavigate } from 'react-router';
 import { otherProjects } from './projectData';
 import { useState } from 'react';
 import '../styles/Projects.css';
 
 function Other() {
   const [projects, setProjects] = useState(otherProjects);
+  const navi = useNavigate();
 
   function imgPrev(idx: number) {
     setProjects((prev) => {
@@ -27,6 +29,8 @@ function Other() {
         <p style={{ opacity: '0.7', margin: '0px' }}>
           Also, the Vanilla JavaScript projects are really basic — they were of course my first projects.
         </p>
+      <button className='other-projects-btn' onClick={() => navi('/projects')}>Go Back</button>
+
         {projects.map((proj, i) => (
           <div key={i}>
             {!proj.isActive && (
@@ -42,9 +46,9 @@ function Other() {
             )}
 
             {proj.isActive && (
-              <div className="overlay">
+              <div className="overlay" onClick={() => imgPrev(i)}>
                 <div className="wrapper">
-                  <div className='image-preview-cont'>
+                  <div className='image-preview-cont' onClick={(e) => e.stopPropagation()}>
                   <ImgPreview
                     title={proj.title}
                     desc={proj.desc}
